dfd6be5f37d5cba0a071906f0f7952186805585f
Spencer Carli <spencercarli@gmail.com>
2017-01-17 13:38:58 -0600

    Request location permission

--- react-native/app/screens/FindNearMe.js
+++ react-native/app/screens/FindNearMe.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
+import LocateMeButton from '../components/LocateMeButton';
+import config from '../config/config';
 
 class FindNearMe extends Component {
   static route = {
@@ -9,11 +11,35 @@ class FindNearMe extends Component {
     },
   }
 
+  static propTypes = {
+    navigator: PropTypes.object,
+  }
+
+  handleGeolocationSuccess = (position) => {
+    console.log('latitude', position.coords.latitude);
+    console.log('longitude', position.coords.longitude);
+  };
+
+  handleGeolocationError = (error) => {
+    this.props.navigator.showLocalAlert(error.message, config.errorStyles);
+  };
+
+  goToNearMe = () => {
+    navigator.geolocation.getCurrentPosition(
+      this.handleGeolocationSuccess,
+      this.handleGeolocationError,
+      { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
+    );
+  };
+
   render() {
     return (
       <Container>
+        <LocateMeButton
+          onPress={this.goToNearMe}
+        />
         <Header>
-          Find Near Me
+          Find Nearest Charging Stations
         </Header>
       </Container>
     );
