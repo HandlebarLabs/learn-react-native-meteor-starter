633ba0f20efe2bd468aacdaeecbc4712be808f70
Spencer Carli <spencercarli@gmail.com>
2017-01-18 14:02:11 -0600

    Remove meteor code we'll be adding later

--- meteor/imports/api/activity/server/publications.js
+++
@@ -1,6 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { Activity } from '../activity';
-
-Meteor.publish('Activity.pub.list', function getLocationActivity({ locationId }) {
-  return Activity.find({ locationId }, { limit: 5, sort: { createdAt: -1 } });
-});
--- meteor/imports/api/locations/server/publications.js
+++
@@ -1,6 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { Locations } from '../locations';
-
-Meteor.publish('Locations.pub.details', function getLocationDetails({ locationId }) {
-  return Locations.find({ _id: locationId });
-});
