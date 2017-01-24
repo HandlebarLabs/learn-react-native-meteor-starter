49e1541dc31be33738ffcbac632548ff19e05fbe
Spencer Carli <spencercarli@gmail.com>
2017-01-24 13:15:22 -0600

    Ability to check in/out

--- meteor/imports/api/locations/methods.js
+++ meteor/imports/api/locations/methods.js
@@ -1,3 +1,4 @@
+import { Meteor } from 'meteor/meteor';
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 import { Locations } from './locations';
@@ -22,5 +23,37 @@ export const getNearestLocations = new ValidatedMethod({
     };
 
     return Locations.find(query, { limit: 10 }).fetch();
+  },
+});
+
+export const changeCheckinStatus = new ValidatedMethod({
+  name: 'Locations.changeCheckin',
+  validate: new SimpleSchema({
+    locationId: { type: String },
+    status: { type: String, allowedValues: ['in', 'out'] },
+  }).validator(),
+  run({ locationId, status }) {
+    const location = Locations.findOne({ _id: locationId });
+
+    if (!location) {
+      throw new Meteor.Error('Locations.changeCheckin.invalidLocationId',
+        'Must pass a valid location id to change checkin status.');
+    }
+
+    if (status === 'in') {
+      Locations.update({ _id: locationId }, {
+        $set: {
+          checkedInUserId: 'demo', // TEMPORARY
+          updatedAt: new Date(),
+        },
+      });
+    } else {
+      Locations.update({ _id: locationId }, {
+        $set: {
+          checkedInUserId: null,
+          updatedAt: new Date(),
+        },
+      });
+    }
   },
 });
--- react-native/app/screens/LocationDetails.js
+++ react-native/app/screens/LocationDetails.js
@@ -5,14 +5,42 @@ import _ from 'lodash';
 import Meteor, { createContainer } from 'react-native-meteor';
 import Container from '../components/Container';
 import colors from '../config/colors';
+import config from '../config/config';
+
+const CHECKED_IN = 'in';
+const CHECKED_OUT = 'out';
 
 class LocationDetails extends Component {
   static propTypes = {
     location: PropTypes.object,
+    navigator: PropTypes.object,
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      changingStatus: false,
+    };
+  }
+
+  attemptCheckin = () => {
+    const { location } = this.props;
+    let status = CHECKED_IN;
+    if (location.checkedInUserId) {
+      status = CHECKED_OUT;
+    }
+
+    this.setState({ changingStatus: true });
+    Meteor.call('Locations.changeCheckin', { locationId: location._id, status }, (err) => {
+      if (err) {
+        this.props.navigator.showLocalAlert(err.reason, config.errorStyles);
+      }
+      this.setState({ changingStatus: false });
+    });
+  };
+
   render() {
-    console.log(this.props.location);
     const location = this.props.location || _.get(this.props, 'route.params.location', {});
     const userId = _.get(this.props, 'user._id', '');
     const checkedIn = location.checkedInUserId === userId;
@@ -46,6 +74,8 @@ class LocationDetails extends Component {
           backgroundColor={backgroundColor}
           buttonStyle={{ marginVertical: 20 }}
           disabled={!available && !checkedIn}
+          onPress={this.attemptCheckin}
+          loading={this.state.changingStatus}
         />
       </Container>
     );
