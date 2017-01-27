708d3a52ac6dee1a2e2cbab9050c6f64077b3344
Spencer Carli <spencercarli@gmail.com>
2017-01-27 11:45:50 -0600

    Sign in logic

--- react-native/app/screens/SignIn.js
+++ react-native/app/screens/SignIn.js
@@ -1,7 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 import { Card } from 'react-native-elements';
+import Meteor from 'react-native-meteor';
 import Container from '../components/Container';
 import { Input, PrimaryButton } from '../components/Form';
+import config from '../config/config';
+import Router from '../config/router';
 
 class SignIn extends Component {
   static route = {
@@ -9,6 +12,10 @@ class SignIn extends Component {
       visible: true,
       title: 'Sign In',
     },
+  }
+
+  static propTypes = {
+    navigator: PropTypes.object,
   }
 
   constructor(props) {
@@ -19,6 +26,28 @@ class SignIn extends Component {
       password: '',
     };
   }
+
+  signIn = () => {
+    const { emailOrUsername, password } = this.state;
+
+    if (emailOrUsername.length === 0) {
+      return this.props.navigator.showLocalAlert('Email or username is required.', config.errorStyles);
+    }
+
+    if (password.length === 0) {
+      return this.props.navigator.showLocalAlert('Password is required.', config.errorStyles);
+    }
+
+    this.setState({ loading: true });
+    return Meteor.loginWithPassword(emailOrUsername, password, (err) => {
+      this.setState({ loading: false });
+      if (err) {
+        this.props.navigator.showLocalAlert(err.reason, config.errorStyles);
+      } else {
+        this.props.navigator.immediatelyResetStack([Router.getRoute('profile')]);
+      }
+    });
+  };
 
   render() {
     return (
@@ -40,6 +69,7 @@ class SignIn extends Component {
           />
           <PrimaryButton
             title="Sign In"
+            onPress={this.signIn}
           />
         </Card>
       </Container>
