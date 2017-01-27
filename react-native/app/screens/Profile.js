90296676edad7f1efce95d4c47907b9e724badd1
Spencer Carli <spencercarli@gmail.com>
2017-01-27 11:13:09 -0600

    Logout logic

--- react-native/app/screens/Profile.js
+++ react-native/app/screens/Profile.js
@@ -1,6 +1,9 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import Meteor from 'react-native-meteor';
+import Router from '../config/router';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
+import { PrimaryButton } from '../components/Form';
 
 class Profile extends Component {
   static route = {
@@ -10,12 +13,25 @@ class Profile extends Component {
     },
   }
 
+  static propTypes = {
+    navigator: PropTypes.object,
+  }
+
+  signOut = () => {
+    Meteor.logout();
+    this.props.navigator.immediatelyResetStack([Router.getRoute('signUp')]);
+  };
+
   render() {
     return (
       <Container>
         <Header>
           Profile
         </Header>
+        <PrimaryButton
+          title="Sign Out"
+          onPress={this.signOut}
+        />
       </Container>
     );
   }
