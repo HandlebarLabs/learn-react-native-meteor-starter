0f78535c321904458f0afd96c9e78165ba24c319
Spencer Carli <spencercarli@gmail.com>
2017-01-26 20:57:39 -0600

    Compose the sign in and sign up screens

--- react-native/app/screens/SignIn.js
+++ react-native/app/screens/SignIn.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
+import { Card } from 'react-native-elements';
 import Container from '../components/Container';
-import { Header } from '../components/Text';
+import { Input, PrimaryButton } from '../components/Form';
 
 class SignIn extends Component {
   static route = {
@@ -10,12 +11,37 @@ class SignIn extends Component {
     },
   }
 
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      emailOrUsername: '',
+      password: '',
+    };
+  }
+
   render() {
     return (
-      <Container>
-        <Header>
-          Sign In
-        </Header>
+      <Container scroll>
+        <Card>
+          <Input
+            label="Email or Username"
+            placeholder="Please enter your email or username..."
+            keyboardType="email-address"
+            onChangeText={(emailOrUsername) => this.setState({ emailOrUsername })}
+            value={this.state.emailOrUsername}
+          />
+          <Input
+            label="Password"
+            placeholder="Please enter your password..."
+            secureTextEntry
+            onChangeText={(password) => this.setState({ password })}
+            value={this.state.password}
+          />
+          <PrimaryButton
+            title="Sign In"
+          />
+        </Card>
       </Container>
     );
   }
--- react-native/app/screens/SignUp.js
+++ react-native/app/screens/SignUp.js
@@ -1,6 +1,8 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
+import { Card } from 'react-native-elements';
 import Container from '../components/Container';
-import { Header } from '../components/Text';
+import { Input, PrimaryButton, SecondaryButton } from '../components/Form';
+import Router from '../config/router';
 
 class SignUp extends Component {
   static route = {
@@ -10,12 +12,75 @@ class SignUp extends Component {
     },
   }
 
+  static propTypes = {
+    navigator: PropTypes.object,
+  }
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    };
+  }
+
+  handleChangeEmail = (email) => {
+    const { username } = this.state;
+    const update = { email };
+    const inferredUsername = email.split('@')[0];
+    if (username === inferredUsername.slice(0, inferredUsername.length - 1)) {
+      update.username = inferredUsername;
+    }
+    this.setState(update);
+  };
+
+  goToSignIn = () => {
+    this.props.navigator.push(Router.getRoute('signIn'));
+  };
+
   render() {
     return (
-      <Container>
-        <Header>
-          Sign In
-        </Header>
+      <Container scroll>
+        <Card>
+          <Input
+            label="Email"
+            placeholder="Please enter your email..."
+            keyboardType="email-address"
+            onChangeText={this.handleChangeEmail}
+            value={this.state.email}
+          />
+          <Input
+            label="Username"
+            placeholder="Please enter your username..."
+            onChangeText={(username) => this.setState({ username })}
+            value={this.state.username}
+          />
+          <Input
+            label="Password"
+            placeholder="Please enter your password..."
+            secureTextEntry
+            onChangeText={(password) => this.setState({ password })}
+            value={this.state.password}
+          />
+          <Input
+            label="Confirm Password"
+            placeholder="Please enter confirm your password..."
+            secureTextEntry
+            onChangeText={(confirmPassword) => this.setState({ confirmPassword })}
+            value={this.state.confirmPassword}
+          />
+          <PrimaryButton
+            title="Sign Up"
+          />
+        </Card>
+
+        <SecondaryButton
+          title="Sign In"
+          onPress={this.goToSignIn}
+        />
       </Container>
     );
   }
