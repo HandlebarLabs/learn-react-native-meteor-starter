0a98ae482bf1b0d628f2f4ac7d036d486d2fac99
Spencer Carli <spencercarli@gmail.com>
2017-08-24 19:53:10 -0500

    Proptypes codemod

--- react-native/app/screens/Profile.js
+++ react-native/app/screens/Profile.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import Meteor from 'react-native-meteor';
 import { NavigationActions } from 'react-navigation';
 import Container from '../components/Container';
