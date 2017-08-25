f56c5a2196420569494d5bf805697eac62c571e1
Spencer Carli <spencercarli@gmail.com>
2017-08-24 19:52:38 -0500

    Proptypes codemod

--- react-native/app/screens/SignUp.js
+++ react-native/app/screens/SignUp.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import { Card } from 'react-native-elements';
 import Container from '../components/Container';
 import { Input, PrimaryButton, SecondaryButton } from '../components/Form';
