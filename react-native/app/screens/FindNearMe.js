fd43d8fbd47451b37728c0f5215f73abfcbe31b9
Spencer Carli <spencercarli@gmail.com>
2017-08-24 19:49:51 -0500

    Proptypes codemod

--- react-native/app/screens/FindNearMe.js
+++ react-native/app/screens/FindNearMe.js
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import Container from '../components/Container';
 import { Header } from '../components/Text';
 import LocateMeButton from '../components/LocateMeButton';
